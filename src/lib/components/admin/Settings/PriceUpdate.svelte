1e8599f108e4cd1866c4cd5b89d4c5035f6ec1ac
Goeste <320889+Goeste@users.noreply.github.com>
2026-03-01 11:58:07 +0100

    Create PriceUpdate.svelte

---
+++ src/lib/components/admin/Settings/PriceUpdate.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+    import SettingsGroup from "../SettingsGroup.svelte";
+    import Setting from "../Setting.svelte";
+    import { getFormatter } from "$lib/i18n";
+
+    interface Props {
+        config: Pick<Config, "priceUpdate">;
+        hidden?: boolean;
+    }
+
+    const { config, hidden = false }: Props = $props();
+    const t = getFormatter();
+</script>
+
+<div class={{ hidden, "flex flex-col gap-4": !hidden }}>
+    <h2 class="h2">{$t("admin.price-update")}</h2>
+
+    <SettingsGroup title={$t("admin.price-update-automatic")}>
+        <Setting>
+            <label class="checkbox-label">
+                <input
+                    id="enablePriceUpdate"
+                    name="enablePriceUpdate"
+                    class="checkbox"
+                    type="checkbox"
+                    checked={config.priceUpdate.enable}
+                    onchange={(e) => (config.priceUpdate.enable = e.currentTarget.checked)}
+                />
+                <span>{$t("admin.price-update-enable")}</span>
+            </label>
+
+            {#snippet description()}
+                {$t("admin.price-update-enable-tooltip")}
+            {/snippet}
+        </Setting>
+
+        <Setting>
+            <label class="label w-full">
+                <span>{$t("admin.price-update-interval-hours")}</span>
+                <input
+                    id="priceUpdateIntervalHours"
+                    name="priceUpdateIntervalHours"
+                    class="input"
+                    type="number"
+                    min="1"
+                    max="8760"
+                    value={config.priceUpdate.intervalHours}
+                    disabled={!config.priceUpdate.enable}
+                />
+            </label>
+
+            {#snippet description()}
+                {$t("admin.price-update-interval-hours-tooltip")}
+            {/snippet}
+        </Setting>
+
+        <Setting>
+            <label class="label w-full">
+                <span>{$t("admin.price-update-scheduled-time")}</span>
+                <input
+                    id="priceUpdateScheduledTime"
+                    name="priceUpdateScheduledTime"
+                    class="input"
+                    type="time"
+                    value={config.priceUpdate.scheduledTime ?? ""}
+                    disabled={!config.priceUpdate.enable}
+                />
+            </label>
+
+            {#snippet description()}
+                {$t("admin.price-update-scheduled-time-tooltip")}
+            {/snippet}
+        </Setting>
+    </SettingsGroup>
+</div>
