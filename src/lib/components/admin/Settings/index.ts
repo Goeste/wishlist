29e6cbfb8a2c94c5e9c32e62360d0d2133cdd876
Goeste <320889+Goeste@users.noreply.github.com>
2026-03-01 11:58:39 +0100

    Update index.ts

--- src/lib/components/admin/Settings/index.ts
+++ src/lib/components/admin/Settings/index.ts
@@ -1,12 +1,14 @@
 import Email from "./Email";
 import General from "./General";
 import Security from "./Security";
+import PriceUpdate from "./PriceUpdate.svelte";
 import type { MessageFormatter } from "$lib/server/i18n";
 
 const options = [
     { label: ($t: MessageFormatter) => $t("admin.general"), hash: "#general" },
     { label: ($t: MessageFormatter) => $t("auth.email"), hash: "#email" },
-    { label: ($t: MessageFormatter) => $t("admin.security"), hash: "#security" }
+    { label: ($t: MessageFormatter) => $t("admin.security"), hash: "#security" },
+    { label: ($t: MessageFormatter) => $t("admin.price-update"), hash: "#price-update" }
 ];
 
-export { Email, General, Security, options };
+export { Email, General, Security, PriceUpdate, options };
